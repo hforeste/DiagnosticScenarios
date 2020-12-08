936fc0155cd2e0795028bedbd6ed60fa80051dbb
Hawk Foreste <hawfor@microsoft.com>
2020-12-07 16:26:05 -0800

    Add buggy core project

---
+++ BuggyAspNetCore/BuggyCoreWebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BuggyCoreWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BuggyAspNetCore/BuggyCoreWebApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace BuggyCoreWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
